5d9b5adb0b115fef5da4f92ba9befdb03621495a
Vicky-0111 <sanzujack7@gmail.com>
2026-08-12 07:32:49 +0530

    Configure frontend API for Render backend

--- SHIPTRACK_JAVA/shiptrack-frontend/src/api/axiosConfig.js
+++ SHIPTRACK_JAVA/shiptrack-frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8081/api"
+  baseURL: "https://shiptrack-backend-tiul.onrender.com/api",
 });
 
 export default api;
